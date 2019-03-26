f43b5f29b47633dc5a0b5c6aa046fef2c2881b04
yuittti <kozachenkos@gmail.com>
2019-03-26 17:32:17 +0200

    Add basic actions, reducers and sagas

---
+++ src/actions.js
@@ -0,0 +1,15 @@
+export const t = {
+    LOAD_USER_DATA: 'LOAD_USER_DATA',
+    LOAD_USER_DATA_SUCCESS: 'LOAD_USER_DATA_SUCCES'
+};
+
+export const actions = ({
+    loadUserData: name => ({
+        type: t.LOAD_USER_DATA,
+        name
+    }),
+    loadUserDataSuccess: data => ({
+        type: t.LOAD_USER_DATA_SUCCESS,
+        data
+    })
+});
--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { userReducer } from './reducer';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+import { watchLoadUserData } from './saga';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(combineReducers({
+    user: userReducer
+}), applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(watchLoadUserData);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
